4e17f0af7d61966d565422ae9950f210d1842b1b
Ménager Yohan <yohan.menager0600@gmail.com>
2023-11-29 11:30:35 +0100

    création du projet

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module sio.leo.direction.des.sports {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens sio.leo.direction.des.sports to javafx.fxml;
+    exports sio.leo.direction.des.sports;
+}
