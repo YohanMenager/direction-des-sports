409c209ac1bf24f55182c2ebb2a1c3e4596c8fa9
Ménager Yohan <yohan.menager0600@gmail.com>
2023-11-29 11:45:33 +0100

    retour du dossier fxml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module sio.leo.direction.des.sports {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens sio.leo.direction.des.sports to javafx.fxml;
     exports sio.leo.direction.des.sports;
