10f30ff1cd15dcfe7d1ab69aab56ae288779ef1a
MENAGER Yohan <MENAGER@bts.leo>
2023-12-07 11:02:25 +0100

    git casse vraiment les couilles

--- src/main/java/sio/leo/direction/des/sports/App.java
+++ src/main/java/sio/leo/direction/des/sports/App.java
@@ -25,7 +25,7 @@ public class App extends Application {
     
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("AccueilConnexion"), 640, 480);
+        scene = new Scene(loadFXML("MdpOubli"), 640, 480);
         stage.setScene(scene);
         stage.show();
         DAO.SeConnecter();
